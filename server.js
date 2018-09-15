bd8de14fcb4bb8dac1eb002ed7195c71f983f616
smiley-coyote <nick.a.alex@gmail.com>
2018-09-15 16:27:04 -0400

    process env port

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 
 // Initialize Express
 var app = express();
@@ -27,7 +28,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/nintendodb", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nintendodb";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 // Routes
 
